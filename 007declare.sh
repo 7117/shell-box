5baa8c373fb662515e5ab3918afd8b045a82b032
7117 <qinglangsalaheiyou@163.com>
2020-05-29 00:18:57 +0800

    declare

--- 007declare.sh
+++ 007declare.sh
@@ -3,3 +3,4 @@ a=11;b=22;declare -i cc=$a+$b;echo $cc
 #
 #[root@localhost shellCollection]# sh 007declare.sh
 #33
+#使用declare进行计算值的
--- 010.expr.sh
+++ 010.expr.sh
@@ -1,3 +1,4 @@
 aa=11;bb=22;dd=$(expr $aa + $bb);echo $dd;
 #[root@localhost shellCollection]# sh 010.expr.sh
 #33
+#+左右两边都要加上空格！不然计算不行！！
