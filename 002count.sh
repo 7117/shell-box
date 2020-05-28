3fcd94b0a1fc245668d8a486af91b853e8d77afa
7117 <qinglangsalaheiyou@163.com>
2020-05-28 23:01:15 +0800

    init

---
+++ 002count.sh
@@ -0,0 +1,12 @@
+num1=$1
+num2=$2
+
+sum=$(($num1 + $num2))
+
+echo $sum
+
+
+
+
+#[root@localhost shell]# sh count.sh 1 2
+#3
---
+++ 003geshu.sh
@@ -0,0 +1,15 @@
+echo "geshu $#  canshu"
+
+echo "suoyou canshu $*"
+
+echo "dandu  suyoude1   $@"
+
+
+
+
+#
+#[root@localhost shell]# sh 003geshu.sh 3 4 622  654 aq
+#geshu 5  canshu
+#suoyou canshu 3 4 622 654 aq
+#dandu  suyoude1   3 4 622 654 aq
+#[root@localhost shell]
