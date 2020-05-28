8fb8652d2fdc656cd45c347a7f8ed30b9585a22a
7117 <qinglangsalaheiyou@163.com>
2020-05-29 00:07:22 +0800

    $[] $()

---
+++ 012.$().sh
@@ -0,0 +1,4 @@
+aa=11;bb=22;ff=$(($aa+$bb));echo $ff
+#
+#[root@localhost shellCollection]# sh 012.\$\(\).sh
+#33
---
+++ 013.$[].sh
@@ -0,0 +1,3 @@
+aa=11;bb=22;ff=$[$aa+$bb];echo $ff
+#[root@localhost shellCollection]# sh 013.\$\[\].sh
+#33
