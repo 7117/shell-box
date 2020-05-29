1f79ecc9ac71faa94e83c30b9142f50162c37af2
7117 <qinglangsalaheiyou@163.com>
2020-05-29 19:32:23 +0800

    sed

---
+++ 039.sed e.sh
@@ -0,0 +1,6 @@
+#[root@localhost shellCollection]# sed 's/Liming//g;s/Tg//g' stu2.txt
+#ID      Name  PHP       Linux   MySQL   Average
+#1         82  95        86  87.66
+#
+#2       Sc    74        96  87  85.66
+#3         99  83        93  91.66
